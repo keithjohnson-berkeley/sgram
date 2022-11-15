759bee0fb16c1ca46a14194312aadf05c32e4bc0
Keith Johnson <keithjohnson@berkeley.edu>
2022-11-15 15:27:13 -0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'sgram',
+  version='0.2.0',
+  py_modules = ['sgram'],
+  classifiers = [
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Multimedia :: Sound/Audio :: Speech'
+  ],
+  requires = [
+    'scipy',
+    'matplotlib'   
+]
+
+)
